e9eaa140afbe4136b132c9629aa6941b5d60fa5e
Dimitar Dimitrov <mite134@gmail.com>
2017-10-12 15:21:14 +0200

    Adding Field.java

---
+++ model/Field.java
@@ -0,0 +1,25 @@
+public class Field {
+	private String takenBy = "noone";
+	private String name;
+
+	public Field(String name) {
+		this.name=name;
+	}
+
+	public String getTakenBy() {
+		return takenBy;
+	}
+
+	public void setTakenBy(String takenBy) {
+		this.takenBy = takenBy;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
